53afd7e185f4ca5ba5af888a73dc19ed83725a7d
Brian Yee <beeyee0401@gmail.com>
2022-12-08 22:21:54 -0600

    initial commit

---
+++ src/main/java/FilterTheSpire/ui/screens/NeowBonusFilterScreen.java
@@ -0,0 +1,16 @@
+package FilterTheSpire.ui.screens;
+
+import com.megacrit.cardcrawl.screens.options.DropdownMenu;
+import com.megacrit.cardcrawl.screens.options.DropdownMenuListener;
+
+public class NeowBonusFilterScreen implements DropdownMenuListener {
+
+
+    public void changedSelectionTo(DropdownMenu dropdownMenu, int i, String s) {
+        if (dropdownMenu == null){
+
+        } else if (dropdownMenu == null){
+
+        }
+    }
+}
